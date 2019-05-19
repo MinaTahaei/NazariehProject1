c9b927786ee2464bf37166997f2d2d3011144c76
Mina Tahaei <bluxixi@outlook.com>
2019-05-19 22:41:26 +0430

    add read file

---
+++ NazariehProject1-96522204/NazariehProject1-96522204/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace NazariehProject1_96522204
+{
+    class Program
+    {
+        public string[] ReadFile()
+        {
+            var lineCount = File.ReadLines(@"..\..\input.txt").Count();
+            string[] SaveArray = new string[lineCount];
+
+            using (StreamReader sr = new StreamReader(@"..\..\input.txt"))
+            {
+
+                string line;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    for (int i = 0; i <= lineCount; i++)
+                    {
+                        SaveArray[i] = sr.ReadLine();
+                    }
+                }
+            }
+            return SaveArray;
+        }
+
+        static void Main()
+        {
+
+        }
+    }
+}
