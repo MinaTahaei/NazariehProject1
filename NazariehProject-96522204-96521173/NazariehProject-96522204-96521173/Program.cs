f7770a83f0183bfeee3b78e30329758ed17bd19a
mahnaz <mahnaz.hagh@outlook.com>
2019-05-24 23:41:13 +0430

    added uncomplete nfa to dfa

--- NazariehProject-96522204-96521173/NazariehProject-96522204-96521173/Program.cs
+++ NazariehProject-96522204-96521173/NazariehProject-96522204-96521173/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-namespace NazariehProject1_96522204
+namespace NazariehProject_96522204_96521173
 {
     class Program
     {
@@ -11,7 +11,7 @@ namespace NazariehProject1_96522204
         {
             List<string> SaveList = new List<string>();
 
-            using (StreamReader sr = new StreamReader(@"..\..\..\input.txt"))
+            using (StreamReader sr = new StreamReader(@"..\..\input.txt"))
             {
 
                 string line;
@@ -26,9 +26,9 @@ namespace NazariehProject1_96522204
             return SaveList.ToArray();
         }
 
-        public static Dictionary<string, Dictionary<string, string>> MakeNFA(string[] data, out string InitState, out HashSet<string> FinalStates)
+        public static Dictionary<string, Dictionary<string, List<string>>> MakeNFA(string[] data, out string InitState, out HashSet<string> FinalStates)
         {
-            Dictionary<string, Dictionary<string, string>> NFA = new Dictionary<string, Dictionary<string, string>>();
+            Dictionary<string, Dictionary<string, List<string>>> NFA = new Dictionary<string, Dictionary<string, List<string>>>();
             InitState = "0";
             FinalStates = new HashSet<string>();
 
@@ -53,31 +53,111 @@ namespace NazariehProject1_96522204
 
                     temp[2] = Finalstate;
                 }
+                if(temp[0][0] == '*')
+                {
+                    var Finalstate = temp[0].Substring(1);
+
+                    FinalStates.Add(Finalstate);
+
+                    temp[0] = Finalstate;
+                }
 
                 if (NFA.ContainsKey(temp[0]))
                 {
-                    NFA[temp[0]].Add(temp[1], temp[2]);
+                    if (NFA[temp[0]].ContainsKey(temp[1]))
+                    {
+                        NFA[temp[0]][temp[1]].Add(temp[2]);
+                    }
+                    else
+                    {
+                        NFA[temp[0]].Add(temp[1], new List<string>() { temp[2] });
+                    }
 
                 }
                 else
                 {
-                    NFA.Add(temp[0], new Dictionary<string, string>());
+                    NFA.Add(temp[0], new Dictionary<string, List<string>>());
 
-                    NFA[temp[0]].Add(temp[1], temp[2]);
+                    NFA[temp[0]].Add(temp[1], new List<string>() { temp[2]});
                 }
             }
             return NFA;
         }
 
-        public static void ConvertToDFA(Dictionary<string, Dictionary<string, string>> NFA)
+        public static void ConvertToDFA(Dictionary<string, Dictionary<string, List<string>>> NFA, string InitState, HashSet<string> FinalState)
         {
+            var initstate = new State(InitState, new Dictionary<string, int>(), new List<string>() { InitState });
+            var states = new List<State>();
+            states.Add(initstate);
+            
+            for (int i = 0; i < states.Count; i++)
+            {
+                var state = states[i];
+                for (int j = 0; j < state.States.Count; j++)
+                {
+                    if (NFA[state.States[j]].ContainsKey("_"))
+                    {
+                        state.States.AddRange(NFA[state.States[j]]["_"]);
+                    }
+                }
+                StateExists(states, state);
+                var nextStates = new Dictionary<string, State>();
+                foreach (var s in state.States)
+                {
+                    foreach (var ns in NFA[s])
+                    {
+                        if (ns.Key != "_")
+                        {
+                            if (nextStates.ContainsKey(ns.Key))
+                            {
+                                nextStates[ns.Key].States.AddRange(ns.Value);
+                            }
+                            else
+                            {
+                                nextStates.Add(ns.Key, new State("", new Dictionary<string, int>(), ns.Value));
+                            }
+                        }
+                    }
+                }
 
+                foreach (var ns in nextStates)
+                {
+                    var index = StateExists(states, ns.Value);
+                    state.NextStates.Add(ns.Key, index);
+                }
+            }
+        }
+
+        public static int StateExists(List<State> states, State state)
+        {
+            for(int i = 0; i < states.Count; i++)
+            {
+                if(states[i].States.Count == state.States.Count)
+                {
+                    bool found = true;
+                    for(int j = 0; j < state.States.Count; j++)
+                    {
+                        if(state.States[j] != states[i].States[j])
+                        {
+                            found = false;
+                        }
+                    }
+                    if(found)
+                    {
+                        return i;
+                    }
+                }
+            }
+            states.Add(state);
+            return states.Count - 1;
         }
 
         static void Main()
         {
             var data = ReadFile();
-            MakeNFA(data, out string InitState, out HashSet<string> FinalStates);
+            var nfa = MakeNFA(data, out string InitState, out HashSet<string> FinalStates);
+
+            ConvertToDFA(nfa, InitState, FinalStates);
         }
     }
 }
---
+++ NazariehProject-96522204-96521173/NazariehProject-96522204-96521173/State.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NazariehProject_96522204_96521173
+{
+    public class State
+    {
+        public string Name { get; set; }
+        public Dictionary<string, int> NextStates;
+        public List<string> States { get; set; }
+        
+        public State(string name, Dictionary<string, int> next, List<string> states)
+        {
+            Name = name;
+            NextStates = next;
+            States = states;
+        }
+    }
+}
